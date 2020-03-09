d53656fd610e6e590015f30b3e15f22073c6d6bb
Valentin <Valio_180@abv.bg>
2020-03-09 22:13:30 +0200

    Create a feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,8 +12,8 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero; 
+  onSelect(selectedHero: Hero): void{
+    this.selectedHero = selectedHero; 
   }
   constructor() {}
 
